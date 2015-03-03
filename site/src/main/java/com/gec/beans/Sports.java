33f5635fc8c176f84c71cccea941219bd8b4d85a
Eric <eric.nguyen@goldenequator.com>
2015-03-03 17:10:22 +0800

    Configured Hippo CMS to allow bulk upload of images + pull out images from folder link

--- site/src/main/java/com/gec/beans/Sports.java
+++ site/src/main/java/com/gec/beans/Sports.java
@@ -3,13 +3,26 @@ package com.gec.beans;
 import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
 import org.hippoecm.hst.content.beans.Node;
 import org.hippoecm.hst.content.beans.standard.HippoDocument;
+import java.util.ArrayList;
 import java.util.List;
 import org.hippoecm.hst.content.beans.standard.HippoGalleryImageSet;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.Item;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryManager;
+import javax.jcr.query.QueryResult;
 
 @HippoEssentialsGenerated(internalName = "scc:sports")
 @Node(jcrType = "scc:sports")
 public class Sports extends BaseDocument {
+	private static final Logger log = LoggerFactory.getLogger(Eventscc.class);
+
 	@HippoEssentialsGenerated(internalName = "scc:name")
 	public String getName() {
 		return getProperty("scc:name");
@@ -25,11 +38,6 @@ public class Sports extends BaseDocument {
 		return getProperty("scc:governor");
 	}
 
-	@HippoEssentialsGenerated(internalName = "scc:images")
-	public List<HippoGalleryImageSet> getImages() {
-		return getLinkedBeans("scc:images", HippoGalleryImageSet.class);
-	}
-
 	@HippoEssentialsGenerated(internalName = "scc:convenor")
 	public String getConvenor() {
 		return getProperty("scc:convenor");
@@ -38,5 +46,42 @@ public class Sports extends BaseDocument {
 	@HippoEssentialsGenerated(internalName = "scc:gallery")
 	public HippoBean getGallery() {
 		return getLinkedBean("scc:gallery", HippoBean.class);
+	}
+
+	public List<HippoGalleryImageSet> getImages() {
+		log.info("getImages");
+		List<HippoGalleryImageSet> images = new ArrayList<HippoGalleryImageSet>();
+		String expression = "/jcr:root/content/gallery/scc/gallery1//element(*, hippo:handle)";
+		try {
+			Session session = getNode().getSession();
+			QueryManager queryManager = session.getWorkspace()
+					.getQueryManager();
+			String language = Query.XPATH;
+			Query query = queryManager.createQuery(expression, language);
+			QueryResult result = query.execute();
+			NodeIterator nodes = result.getNodes();
+			while (nodes.hasNext()) {
+				javax.jcr.Node node = nodes.nextNode();
+				log.info("node is "+node);
+
+				// Item item = node.getPrimaryItem();
+				HippoGalleryImageSet image = new HippoGalleryImageSet();
+				image.setNode(node);
+
+
+				images.add(image);
+			}
+		} catch (RepositoryException e) {
+			log
+					.error(
+							"Failed to retrieve average rating for product document {}",
+							getCanonicalHandleUUID(), e);
+		}
+		return images;
+	}
+
+	@HippoEssentialsGenerated(internalName = "scc:Image")
+	public HippoGalleryImageSet getImage() {
+		return getLinkedBean("scc:Image", HippoGalleryImageSet.class);
 	}
 }
--- site/src/main/webapp/WEB-INF/freemarker/scc/sportspage-main.ftl
+++ site/src/main/webapp/WEB-INF/freemarker/scc/sportspage-main.ftl
@@ -2,17 +2,17 @@
 <h2>Sports Page</h2>
 <#if document??>
     <@hst.link var="link" hippobean=document/>
-<div class="yui-main">
-    <div id="content" class="yui-b left-and-right">
+<div class="">
+    <div id="content" class="">
         <div>
             <@hst.cmseditlink hippobean=document/>
-            <h2>${document.title}</h2>
-            <p>${document.gallery}</p>
-            <div id="product" class="product-item">
-                <#if document.gallery.images??>
+            <h2>${document.name}</h2>
+            <p>${document.images}</p>
+            <div id="" class="">
+                <#if document.images??>
                     <div class="clear">
                         <ul id="gallery">
-                            <#list document.gallery.images as item>
+                            <#list document.images as item>
                                 <@hst.link var="img" hippobean=item/>
                                 <li>
                                     <img src="${img}" alt="${item.fileName}" />
@@ -21,20 +21,8 @@
                         </ul>
                     </div>
                 </#if>
-                <#if document.date??>
-                    <span class="date"><@fmt.formatDate value=document.date.time type="both" dateStyle="medium" timeStyle="short"/></span>
-                </#if>
-                <p class="info">
-                    <span class="price"><@fmt.formatNumber value="${document.price}" type="currency" /></span><span class="seperator"><#-- | </span> <span class="rating stars-0">0.0</span>-->
-                </p>
-                <div class="yui-cssbase body">
-                    <div id="editable_cont" class="inline-editor-editable-container">
-                        <span class="" id="hippogogreen:summary">${document.introduction}</span> <span
-                            class=" inline" id="hippogogreen:description">
-                        <@hst.html hippohtml=document.description/>
-                    </span>
-                    </div>
-                </div>
+
+
             </div>
         </div>
     </div>
